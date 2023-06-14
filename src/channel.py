f3be1d7d26ade2ee6595fc769997874a092b4c8d
Maria <maria.mandrykina@gmail.com>
2023-06-14 14:11:23 +0300

    commit channel.py

--- src/channel.py
+++ src/channel.py
@@ -4,9 +4,6 @@ import os
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-import isodate
-
-
 def printj(dict_to_print: dict) -> None:
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
