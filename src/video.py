52746b238568f2e69f0770ac16ac0802f2e64e8a
Maria <maria.mandrykina@gmail.com>
2023-06-27 00:14:44 +0300

    commit class Video and class PLVideo

---
+++ src/video.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+# необходимо установить через:
+# pip install google-api-python-client
+from googleapiclient.discovery import build
+
+
+class Video:
+    """
+    Класс для ютуб - канала
+    """
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.getenv('YT_API_KEY')
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    """
+  - id видео - video_id
+  - название видео - title
+  - ссылка на видео - url
+  - количество просмотров - view_count
+  - количество лайков - like_count
+    """
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=','.join([video_id])).execute()
+        self.title = self.video['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/watch?v='+ self.video_id
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, ply_id):
+        super().__init__(video_id)
+        self.ply_id = ply_id
+
